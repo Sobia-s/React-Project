3f20bd602bb11172efcc024e45970957173c4e93
My-First-Account <sobiasoomro97@gmail.com>
2024-06-16 08:06:03 +0500

    adding and updating some files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import './App.css';
 import Calculator from './components/calculator/Calculator';
 import ToDo from './components/todo/ToDo';
 import Portfolio from './components/Portfolio/Portfolio';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import './App.css'
 
 function App() {
-  const location = useLocation();
-  const hideNavOnHome = location.pathname !== '/';
+const location = useLocation();
+const hideNavOnHome = location.pathname !== '/';
 
-  const handleLinkClick = (path) => (event) => {
-    event.preventDefault();
-    window.open(path, '_blank');
-  };
+const handleLinkClick = (path) => (event) => {
+event.preventDefault();
+window.open(path, '_blank');
+};
 
-  return (
-    <>
-      {!hideNavOnHome && (
-        <div className='nav-container'>
-          <div className='header'>
-            <h2>My React Projects</h2>
-          </div>
-          <nav className='nav'>
-            <Link to="/calculator" className='nav-items'>Calculator</Link>
-            <Link to="/to-do" className='nav-items'>To-Do</Link>
-            <Link to="/portfolio" className='nav-items'>Portfolio Page</Link>
-          </nav>
-        </div>
-      )}
-
-      <Routes>
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/to-do" element={<ToDo />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-      </Routes>
-    </>
-  );
+return (
+<>
+{!hideNavOnHome && (
+<div className='nav-container'>
+<div className='header'>
+<h2>My React Projects</h2>
+</div>
+<nav className='nav'>
+<a href="/calculator" onClick={handleLinkClick("/calculator")} className='nav-items'>Calculator</a>
+<a href="/to-do" onClick={handleLinkClick("/to-do")} className='nav-items'>To-Do</a>
+<a href="/portfolio" onClick={handleLinkClick("/portfolio")} className='nav-items'>Portfolio Page</a>
+</nav>
+</div>
+)}
+  <Routes>
+    <Route path="/calculator" element={<Calculator />} />
+    <Route path="/to-do" element={<ToDo />} />
+    <Route path="/portfolio" element={<Portfolio />} />
+  </Routes>
+</>
+);
 }
 
 export default App;
