cff72bcfa62715cd370c488f4b02f3e04af61176
My-First-Account <sobiasoomro97@gmail.com>
2024-07-19 11:34:11 +0500

    making some changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+import Portfolio from './Portfolio/Portfolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Portfolio />
     </BrowserRouter>
   </React.StrictMode>
 );
